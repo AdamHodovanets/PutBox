2549b93e24280bb8e310f3aab92c5c88156d06b9
Adam Hodovanets <adamhodovanets@gmail.com>
2018-04-02 20:39:35 +0300

    Added FtpManager

---
+++ PutBoxService/PutBoxDbManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PutBox;
+
+namespace PutBoxService
+{
+    internal interface IPutBoxDbManager
+    {
+        bool Register(string email, string password);
+        bool Login(string email, string password);
+
+    }
+    public class PutBoxDbManager : IPutBoxDbManager
+    {
+        public bool Register(string email, string password)
+        {
+            using (var db = new PutBoxModel())
+            {
+                if (db.UserDatas.Any(x => x.email == email)) return false;
+                 // credentials
+                db.UserDatas.Add(new UserData()
+                {
+                    email = email,
+                    password = password.GetHashCode().ToString(),
+                    id = GetId(),
+                    path = "TODO"
+                });
+                db.SaveChanges();
+            }
+                return true;
+        }
+
+        public bool Login(string email, string password)
+        {
+            using (var db = new PutBoxModel())
+                return db.UserDatas.Any(x => x.email == email 
+                        && x.password == password.GetHashCode().ToString());
+        }
+
+        private static int GetId()
+        {
+            bool IsIdExists(int id)
+            {
+                using (var db = new PutBoxModel())
+                {
+                    return db.FileDatas.Any(x => x.id == id) ||
+                           db.FolderDatas.Any(x => x.id == id) ||
+                           db.UserDatas.Any(x => x.id == id);
+                }
+            }
+
+            using (var db = new PutBoxModel())
+            {
+                var size = db.UserDatas.Count() + db.FileDatas.Count() + db.FolderDatas.Count();
+                while (true)
+                {
+                    var tmpId = new Random().Next(0,size + 1);
+                    if (!IsIdExists(tmpId))
+                        return tmpId;
+                }
+            }
+        }
+    }
+}
